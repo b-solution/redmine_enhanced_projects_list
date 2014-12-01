45c1a60635bc36ecc0d184117128454732a6ee1e
Bilel KEDIDI <bilel.kedidi@gmail.com>
2014-12-01 12:05:31 +0100

    feat: add filter, create table html, create config page in admin/setting and in sidebar

--- init.rb
+++ init.rb
@@ -1,8 +1,18 @@
 Redmine::Plugin.register :redmine_enhanced_projects_list do
   name 'Redmine Enhanced Projects List plugin'
-  author 'Author name'
+  author 'Bilel KEDIDI'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'http://example.com/path/to/plugin'
-  author_url 'http://example.com/about'
+  url 'https://github.com/bilel-kedidi/redmine_enhanced_projects_list'
+  author_url 'https://github.com/bilel-kedidi'
+
+  settings :default => {
+      'name'  => true,
+      'description'     => true,
+      'created_on'         => true,
+      'update_on' => true
+  }, :partial => 'settings/setting'
+end
+Rails.application.config.to_prepare do
+  ProjectsController.send(:include, Patches::ProjectsControllerPatch)
 end

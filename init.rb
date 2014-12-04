1e9cdc896fc0605be64d70490d75d8df3f4e23f3
Bilel KEDIDI <bilel.kedidi@gmail.com>
2014-12-04 09:06:35 +0100

    final project redmine enhanced projects lists

--- init.rb
+++ init.rb
@@ -3,17 +3,18 @@ Redmine::Plugin.register :redmine_enhanced_projects_list do
   author 'Bilel KEDIDI'
   description 'This is a plugin for Redmine'
   version '0.0.1'
-  url 'https://github.com/bilel-kedidi/redmine_enhanced_projects_list'
-  author_url 'https://github.com/bilel-kedidi'
+  url 'https://github.com/Atrasoftware/redmine-enhanced-projects-list'
 
+  permission :copy, :projects => :copy
   settings :default => {
       'name'  => true,
       'description'     => true,
       'created_on'         => true,
-      'update_on' => true
+      'update_on' => true,
+      'sorting_projects_order'=> false
   }, :partial => 'settings/setting'
 end
 Rails.application.config.to_prepare do
   ProjectsController.send(:include, Patches::ProjectsControllerPatch)
-
+  ProjectsHelper.send(:include, Patches::ProjectsHelperPatch)
 end
--- lib/patches/projects_controller_patch.rb
+++ lib/patches/projects_controller_patch.rb
@@ -7,8 +7,9 @@ module  Patches
 
       base.send(:include, InstanceMethods)
       base.class_eval do
-
+        before_filter :require_admin, :only => [ :archive, :unarchive, :destroy ]
         alias_method_chain  :index, :filter_project
+        before_filter :copy_authorize, :only=>[:copy]
       end
     end
   end
@@ -42,22 +43,62 @@ module  Patches
           projects = Project.visible.order('created_on DESC').limit(Setting.feeds_limit.to_i).all
           render_feed(projects, :title => "#{Setting.app_title}: #{l(:label_project_latest)}")
         }
+        format.pdf{
+
+          puts "=====--------------------#{params}--------==========-------------------"
+          scope = Project
+          unless params[:closed]
+            scope = scope.active
+          end
+          @settings = Setting.send "plugin_redmine_enhanced_projects_list"
+          if params[:project_search]
+            scope= scope.visible.where("name like ? or identifier like ? ","%#{params[:project_search]}%","%#{params[:project_search]}%")
+            val = CustomValue.where(:customized_type=> 'Project').where("value like ?", "%#{params[:project_search]}%" )
+            val.each do |pr|
+              scope<< pr.customized unless scope.map(&:identifier).include? pr.customized.identifier
+            end
+          else
+            scope = scope.visible.order('lft').all
+          end
+          @projects = scope
+          settings= Array.new
+          settings << l("field_identifier".to_sym)
+          Project.column_names.select{|col| @settings[col.to_sym] and col!= "identifier" }.each do |column|
+            settings<< l("field_#{column}".to_sym)
+          end
+          CustomField.where(:type=> "ProjectCustomField").order("name ASC").select{|col| @settings[col.name.to_sym] }.each do |cf|
+            settings<< cf.name
+          end
+          order_desc = false
+          if @settings[:sorting_projects_order] == 'true'
+            order_desc = true
+          end
+          @settings= @settings.except(:sorting_projects_order)
+        send_data(ProjectsHelper.to_pdf(@projects,@settings,settings,'FR',order_desc), :type => 'application/pdf', :filename => 'projects.pdf')
+        }
         format.js{
           scope = Project
           unless params[:closed]
             scope = scope.active
           end
-
-          if params[:project_search]
-            @projects = scope.visible.where("name like ? or identifier like ? ","%#{params[:project_search]}%","%#{params[:project_search]}%").order('lft')
-          else
-            if User.current.admin?
-              Setting.send "plugin_redmine_enhanced_projects_list=", params[:settings]
-            end
-            @projects = scope.visible.order('lft').all
-          end
           @settings = Setting.send "plugin_redmine_enhanced_projects_list"
+          if params[:project_search]
+            scope= scope.visible.where("name like ? or identifier like ? ","%#{params[:project_search]}%","%#{params[:project_search]}%")
+            val = CustomValue.where(:customized_type=> 'Project').where("value like ?", "%#{params[:project_search]}%" )
+            val.each do |pr|
+              scope<< pr.customized unless scope.map(&:identifier).include? pr.customized.identifier
+            end
+          end
+          @projects = scope
         }
+      end
+    end
+
+    private
+    def copy_authorize
+      project = Project.find(params[:id])
+      unless User.current.member_of?(project) and User.current.allowed_to?({:controller => :projects, :action => :copy}, project, :global => false)
+          require_admin
       end
     end
   end
